bab01e1f7e6cfe850562fcca99157cb5d0913c53
xyz <xyz3676@yahoo.com>
2021-09-18 23:15:25 +0700

    up

---
+++ QuangCaoVinhAn/Models/CMenu.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuangCaoVinhAn.Models
+{
+    public class CMenu
+    {
+            public string ID_SAN_PHAM { get; set; }
+            public string TEN_SAN_PHAM { get; set; }           
+    }
+}
