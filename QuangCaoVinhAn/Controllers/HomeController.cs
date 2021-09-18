003287bfe41dd78395f8a4ed3123cb7cfd3e4ebf
Nguyễn Tiến Dũng <mr.tiendungnguyen@gmail.com>
2021-09-18 20:46:46 +0700

    Dung

--- QuangCaoVinhAn/Controllers/HomeController.cs
+++ QuangCaoVinhAn/Controllers/HomeController.cs
@@ -1,32 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Newtonsoft.Json;
+
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using QuangCaoVinhAn.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace QuangCaoVinhAn.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public partial class T_THIET_LAP
         {
-            _logger = logger;
+            public string ID_SAN_PHAM { get; set; }
+            public string TEN_SAN_PHAM { get; set; }
+           
+            public Nullable<int> SO_THU_TU { get; set; }
+
+        }
+        public void meta()
+        {
+            using (var webClient = new System.Net.WebClient())
+            {
+                webClient.Encoding = Encoding.UTF8;                
+                var json2 = webClient.DownloadString("http://api.support.vnpage.vn/api/runquery/webportal/quangcaovinhan.com/select * from T_SAN_PHAM");
+                var tb_thiet_lap = JsonConvert.DeserializeObject<List<T_THIET_LAP>>(json2);
+
+                
+                ViewBag.image = tb_thiet_lap[0].TEN_SAN_PHAM;
+            }
         }
 
         public IActionResult Index()
         {
-            return View();
+            //meta();
+            using (var webClient = new System.Net.WebClient())
+            {
+                webClient.Encoding = Encoding.UTF8;
+                var json2 = webClient.DownloadString("http://api.support.vnpage.vn/api/runquery/webportal/quangcaovinhan.com/select * from T_SAN_PHAM");
+                var tb_thiet_lap = JsonConvert.DeserializeObject<List<T_THIET_LAP>>(json2);
+
+            }
+            return View(tb_thiet_lap);
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
