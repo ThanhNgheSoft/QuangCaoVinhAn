03fc457f8b3e192f2e50714c823b7479c1cdbe29
Nguyễn Tiến Dũng <mr.tiendungnguyen@gmail.com>
2021-09-19 11:04:57 +0700

    Dung update

--- QuangCaoVinhAn/Controllers/HomeController.cs
+++ QuangCaoVinhAn/Controllers/HomeController.cs
@@ -15,8 +15,52 @@ namespace QuangCaoVinhAn.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
-        {           
+        public partial class T_THIET_LAP
+        {
+            public string ID_DOANH_NGHIEP { get; set; }
+            public string ID_THIET_LAP { get; set; }
+            public string NOI_DUNG { get; set; }
+            public string LIEN_KET { get; set; }
+            public string GHI_CHU { get; set; }
+            public string TIEU_DE { get; set; }
+            public Nullable<int> SO_THU_TU { get; set; }
+
+        }
+        public void KhoiTao()
+        {
+            using (var webClient = new System.Net.WebClient())
+            {
+                webClient.Encoding = Encoding.UTF8;
+                var json2 = webClient.DownloadString("http://api.land.zgroups.vn/api/nhadatnguyencuong.com/Thongtin/all");
+                var tb_thiet_lap = JsonConvert.DeserializeObject<List<T_THIET_LAP>>(json2);
+
+                foreach (var item in tb_thiet_lap)
+                {
+                    if (item.ID_THIET_LAP == "Tieudeweb")
+                    {
+                        if (item.NOI_DUNG != null)
+                        {
+                            ViewBag.TIEUDEWEB = item.NOI_DUNG.Replace("<p>", "").Replace("</p>", "");
+                        }
+                    }
+                    if (item.ID_THIET_LAP == "Description")
+                    {
+                        if (item.NOI_DUNG != null)
+                        {
+                            ViewBag.description = item.NOI_DUNG.Replace("<p>", "").Replace("</p>", "");
+                        }
+                    }
+                    if (item.ID_THIET_LAP == "Keywords")
+                    {
+                        if (item.NOI_DUNG != null)
+                        {
+                            ViewBag.keywords = item.NOI_DUNG.Replace("<p>", "").Replace("</p>", "");
+                        }
+                    }
+                }
+                ViewBag.image = "/images/avatar-logo.jpg";
+            }
+            //menu
             using (var webClient = new System.Net.WebClient())
             {
                 webClient.Encoding = Encoding.UTF8;
@@ -25,8 +69,42 @@ namespace QuangCaoVinhAn.Controllers
 
                 //var report = JsonConvert.DeserializeObject<List<QuangCaoVinhAn.Models.CMenu>>(json);
                 var table = JsonConvert.DeserializeObject<List<QuangCaoVinhAn.Models.CMenu>>(json);
+                ViewBag.Menu = table;                
+            }
+        }
+        public IActionResult Index()
+        {
+            KhoiTao();
+            using (var webClient = new System.Net.WebClient())
+            {
+                webClient.Encoding = Encoding.UTF8;
+                var json = webClient.DownloadString("http://api.support.vnpage.vn/api/runquery/webportal/quangcaovinhan.com/select ID_SAN_PHAM, TEN_SAN_PHAM from T_SAN_PHAM");
+                //QuangCaoVinhAn.Models.CMenu tb_menu = JsonConvert.DeserializeObject<QuangCaoVinhAn.Models.CMenu>(json);
+
+                //var report = JsonConvert.DeserializeObject<List<QuangCaoVinhAn.Models.CMenu>>(json);
+                var table = JsonConvert.DeserializeObject<List<QuangCaoVinhAn.Models.CMenu>>(json);
+                ViewBag.test = table;
                 return View(table);
-            }                    
+            }
+
+        }
+
+        [Route("Sanpham/{id}")]
+        public IActionResult Sanpham(string id)
+        {
+            KhoiTao();
+            using (var webClient = new System.Net.WebClient())
+            {
+                webClient.Encoding = Encoding.UTF8;
+                var json = webClient.DownloadString("http://api.support.vnpage.vn/api/runquery/webportal/quangcaovinhan.com/select ID_SAN_PHAM, TEN_SAN_PHAM from T_SAN_PHAM");
+                //QuangCaoVinhAn.Models.CMenu tb_menu = JsonConvert.DeserializeObject<QuangCaoVinhAn.Models.CMenu>(json);
+
+                //var report = JsonConvert.DeserializeObject<List<QuangCaoVinhAn.Models.CMenu>>(json);
+                var table = JsonConvert.DeserializeObject<List<QuangCaoVinhAn.Models.CMenu>>(json);
+                ViewBag.test = table;
+                return View(table);
+            }
+
         }
 
 
